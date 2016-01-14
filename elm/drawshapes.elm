0684a48bd0774d92931aea68834b16da6f778b0c
Chris Phelps <cphelps@tendrilinc.com>
2016-01-14 09:23:52 -0700

    Refactor selectShape to return function, add type annotation to functions

--- elm/drawshapes.elm
+++ elm/drawshapes.elm
@@ -8,25 +8,33 @@ import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 
+bluebox: Float -> Float -> Form
 bluebox x y = filled blue (rect 50 50) 
   |> moveX x 
   |> moveY -y
 
+redcircle: Float -> Float -> Form
 redcircle x y = filled red (circle 24)
   |> moveX x
   |> moveY -y
 
+calcx: Int -> Int -> Float
 calcx x w = toFloat x - toFloat w / 2
+
+calcy: Int -> Int -> Float
 calcy y h = toFloat y - toFloat h / 2
 
-selectShape x y down =
-  if down == True then
-    redcircle x y
+selectShape: Bool -> Float -> Float -> Form
+selectShape down =
+  if down then
+    redcircle
   else
-    bluebox x y
+    bluebox
 
-draw (x, y) (w, h) down = collage w h [ selectShape (calcx x w) (calcy y h) down ]
+draw: (Int, Int) -> (Int, Int) -> Bool -> Element
+draw (x, y) (w, h) down = collage w h [ selectShape down (calcx x w) (calcy y h) ]
 
+showxy: (Int, Int) -> (Int, Int) -> Element
 showxy (x,y) (w, h) = show (
   toString x ++ " " ++ 
   toString y ++ " " ++
